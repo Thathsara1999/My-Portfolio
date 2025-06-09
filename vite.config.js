e2552f6c41a055e26a14835f399894d76aa6362b
Nawanka Thathsara <141746388+Thathsara1999@users.noreply.github.com>
2025-06-10 00:20:46 +0530

    refacotered

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/My-Portfolio/", // 👈 This is the fix
+  base: "/My-Portfolio/",
   plugins: [react()],
   css: {
     modules: {
